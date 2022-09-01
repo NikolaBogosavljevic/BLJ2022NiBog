dd5665fb9098c1e0bcb2ea6b08f8f80dde8a4de7
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2022-09-01 15:20:24 +0200

    schaltjahr berechnung verbessert

--- C/KW35/DatumAufgabe/main.c
+++ C/KW35/DatumAufgabe/main.c
@@ -14,9 +14,9 @@ int main() {
     return 0;
 }
 int isLeapYear(int year){
-    if(year & 400 == 0){
+    if(year % 400 == 0){
         return 1;
-    }else if (year & 100 == 0){
+    }else if (year % 100 == 0){
         return 0;
     }
     else if (year % 4 == 0){

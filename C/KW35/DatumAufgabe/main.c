52aa10c85f8417c689bbb160dfff7916ef3dc803
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2022-09-01 15:02:17 +0200

    Fehler verbessert

--- C/KW35/DatumAufgabe/main.c
+++ C/KW35/DatumAufgabe/main.c
@@ -14,11 +14,8 @@ int main() {
     return 0;
 }
 int isLeapYear(int year){
-    if (year % 400 == 0){
-        return 1;
-    }else if (year % 100 == 0){
-        return 0;
-    }else if (year % 4 == 0){
+  
+     if (year % 4 == 0){
         return 1;
     }else{
         return 0;

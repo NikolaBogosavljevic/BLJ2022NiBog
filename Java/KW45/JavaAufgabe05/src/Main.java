147533959bf2c5784e80cfe80e39798af1fc1885
NikolaBogosavljevic <nikola.bogosavljevic@noseryoung.com>
2022-11-10 15:46:53 +0100

    finished Java 05

---
+++ Java/KW45/JavaAufgabe05/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = 0;
+
+        for(int i = 3; i < 28; i++){
+            result += i;
+        }
+        System.out.println(result);
+    }
+}

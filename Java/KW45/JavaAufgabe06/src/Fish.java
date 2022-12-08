de4894a4a63aae7f945655bd79b8e7e4697f7ff6
NikolaBogosavljevic <nikola.bogosavljevic@noseryoung.com>
2022-12-08 15:44:32 +0100

    wrote recipe

--- Java/KW45/JavaAufgabe06/src/Fish.java
+++ Java/KW45/JavaAufgabe06/src/Fish.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Fish {
+public class Fish {
+    private String name;
+    private int positionX;
+    private int positionY;
+
+    public Fish(String name, int positionX, int positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.name = name;
+    }
+
+
 }

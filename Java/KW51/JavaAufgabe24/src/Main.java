1f33060365c8cc91ace341b5fe04c81361c9306f
Nikola Bogosavljevic <nikola.bogosavljevic@noseryoung.com>
2023-02-02 10:49:47 +0100

    d

--- Java/KW51/JavaAufgabe24/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StepCounter step = new StepCounter("1.9.2017");
-        step.incrementSteps(10000);
-        System.out.println("Am " + step.getDate() + " bin ich " + step.getSteps() + " Schritte gegangen");
-    }
-}
--- Java/KW51/JavaAufgabe25/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String pwd_first = "PassWD15!!";
-        String pwd_weak = "1234567890";
-        String pwd_strong = "NewPWD16!!";
-    }
-}
